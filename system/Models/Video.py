0ff52d42cf6a6f48e3691b6c1c58cb9a571880cb
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-25 21:48:48 +0530

    Added the thumbnail fild

--- system/Models/Video.py
+++ system/Models/Video.py
@@ -4,6 +4,7 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20),nullable = False)
     description = db.Column(db.Text,nullable=False)
+    thumbnail = db.Column(db.String(30),nullable=False)
     created_on = db.Column(db.DateTime, default = datetime.now())
     author = db.Column(db.Integer,db.ForeignKey("user.id",onupdate="CASCADE", ondelete="CASCADE"),nullable=False)
     playlist = db.relationship("Playlist",lazy="dynamic",backref = "playlist")
